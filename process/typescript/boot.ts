b5b28f44161ac578652d2187074d58c20156406a
Ray Villalobos <ray@lynda.com>
2016-02-11 00:01:12 -0500

    02_02e separating our modules

--- process/typescript/boot.ts
+++ process/typescript/boot.ts
@@ -1,13 +1,4 @@
-import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
-
-@Component({
-  selector: 'my-app',
-  template: '<h1>Welcome to my App</h1>'
-})
-
-class AppComponent {
-
-}
+import {AppComponent} from './app.component';
 
 bootstrap(AppComponent);
