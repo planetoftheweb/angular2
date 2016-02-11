869ae88e24f63b5dca97a0152b0702c6de8c89cd
Ray Villalobos <ray@lynda.com>
2016-02-10 23:53:28 -0500

    02_02e

---
+++ process/typescript/boot.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Welcome to my App</h1>'
+})
+
+class AppComponent {
+
+}
+
+bootstrap(AppComponent);
