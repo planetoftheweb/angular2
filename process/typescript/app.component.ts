7d0861a85013b28cefd10899e5c0ce1cb6e1fa4d
Ray Villalobos <ray@lynda.com>
2016-02-11 21:55:54 -0500

    02_04e displaying data

--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -2,9 +2,23 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'partials/app.html' 
+  templateUrl: 'partials/app.html'
 })
 
 export class AppComponent {
+  name: string;
+  artists: any;
 
+  constructor() {
+    this.name =  'Ray';
+    this.artists = [
+      {
+          name: 'Barot Bellingham',
+          school: 'Penn State'
+      },{
+          name: 'Hillary Post',
+          school: 'University of Illinois'
+      }
+    ];
+  }
 }
